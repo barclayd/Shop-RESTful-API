4c527f917e25836276394b5d1b62151d6ed5de00
Daniel Barclay <barclaysd@me.com>
2019-01-22 23:52:21 +0000

    Error handling and default logging

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+// error handling for unhandled routes
+app.use((req, res, next) => {
+   const error = new  Error('Path not found');
+   error.status = 404;
+   next(error);
+});
+// handle all errors
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 
 module.exports = app;

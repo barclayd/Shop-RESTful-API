6b0c6472e4b7ccc35f978a7b0af9a3bd1f7b3643
Daniel Barclay <barclaysd@me.com>
2019-01-22 23:38:04 +0000

    Added multiple routes

---
+++ api/routes/orders.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /orders'
+    })
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST requests to /orders'
+    })
+});
+
+router.get('/:orderID', (req, res, next) => {
+    const id = req.params.orderID;
+    res.status(200).json({
+        message: `Handling GET requests to /orders for id: ${id}`,
+        id
+    })
+});
+
+router.delete('/:orderID', (req, res, next) => {
+    const id = req.params.orderID;
+    res.status(202).json({
+        message: `Handling DELETE requests to /orders for id: ${id}`,
+        id
+    })
+});
+
+
+
+module.exports = router;
---
+++ api/routes/products.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+   res.status(200).json({
+       message: 'Handling GET requests to /products'
+   })
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST requests to /products'
+    })
+});
+
+router.get('/:productID', (req, res, next) => {
+    const id = req.params.productID;
+    if(id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an id'
+        })
+    }
+    console.log(id);
+});
+
+router.patch('/:productID', (req, res, next) => {
+    res.status(200).json({
+        message: `Updated product of id: ${req.params.productID}`
+    })
+});
+
+router.delete('/:productID', (req, res, next) => {
+    res.status(200).json({
+        message: `Deleted product of id: ${req.params.productID}`
+    })
+});
+
+module.exports = router;

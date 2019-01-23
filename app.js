1233a5b89dd6d290900a36b2b9321a31a24c15b8
Daniel Barclay <barclaysd@me.com>
2019-01-23 00:10:54 +0000

    Body parser, handling of CORS errors and security settings

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
-
+// middleware
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// CORS settings
+app.use((req, res, next) => {
+    // modify for security settings
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS'){
+        // tell the browser what HTTP methods the API permits
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);

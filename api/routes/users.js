b107f3437e3a36ef03e5d2594d4dd806ac9ad7a2
Daniel Barclay <barclaysd@me.com>
2019-01-24 11:33:57 +0000

    User login with generation of JWT signing

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -48,6 +49,50 @@ router.post('/signup', (req, res, next) => {
         })
 });
 
+router.post('/login', (req, res, next) => {
+   User.find({
+       email: req.body.email
+   })
+       .exec()
+       .then(user => {
+           if(user.length < 1) {
+               return res.status(401).json({
+                   message: 'Login failed'
+               })
+           }
+           bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+              if(err) {
+                  return res.status(401).json({
+                      message: 'Login failed'
+                  });
+              }
+              if(result) {
+                  const token = jwt.sign({
+                      email: user[0].email,
+                      userId: user[0]._id
+                  },
+                      process.env.JWT_KEY,
+                      {
+                          expiresIn: "1h"
+                      });
+                  res.status(200).json({
+                      message: 'Login successful',
+                      token
+                  });
+              } else {
+                  return res.status(401).json({
+                      message: 'Login failed'
+                  });
+              }
+           });
+       })
+       .catch(err => {
+           res.status(500).json({
+               error: err
+           });
+       });
+});
+
 router.delete('/:userId', (req, res, next) => {
    User.remove({
       _id: req.params.userId
@@ -59,11 +104,10 @@ router.delete('/:userId', (req, res, next) => {
            });
        })
        .catch(err => {
-           console.log(err);
            res.status(500).json({
                error: err
            });
-       })
+       });
 });
 
 module.exports = router;
